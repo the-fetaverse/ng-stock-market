284704de90590ddc55875c4671cb0e5f2054a3a7
the-fetaverse <ioannis.sporidis@gmail.com>
2022-12-10 12:51:29 -0500

    End of chapter 3

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Stock } from 'src/app/model/stock';
 
 @Component({
@@ -6,13 +6,35 @@ import { Stock } from 'src/app/model/stock';
   templateUrl: './stock-item.component.html',
   styleUrls: ['./stock-item.component.css'],
 })
-export class StockItemComponent {
+export class StockItemComponent implements OnInit {
   // Values:
-  public stock: Stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+  public stocks: Array<Stock> = [];
+  public stockClasses: any;
+
+  // onInit:
+  ngOnInit(): void {
+    this.stocks = [
+      new Stock('Test Stock Company', 'TSC', 85, 80),
+      new Stock('Second Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 765),
+    ];
+    // let diff = this.stock.price / this.stock.previousPrice - 1;
+    // let largeChange = Math.abs(diff) > 0.01;
+    // this.stockClasses = {
+    //   positive: this.stock.isPositiveChange(),
+    //   negative: !this.stock.isPositiveChange(),
+    //   'large-change': largeChange,
+    //   'small-change': !largeChange,
+    // };
+  }
 
   // Add favorite function:
-  toggleFavorite(event: any) {
-    console.log(event);
-    this.stock.favorite = !this.stock.favorite;
+  toggleFavorite(index: number) {
+    this.stocks[index].favorite = !this.stocks[index].favorite;
+  }
+
+  // Track stocks by stock code:
+  trackStockByCode(index: number, stock: Stock): string {
+    return stock.code;
   }
 }
